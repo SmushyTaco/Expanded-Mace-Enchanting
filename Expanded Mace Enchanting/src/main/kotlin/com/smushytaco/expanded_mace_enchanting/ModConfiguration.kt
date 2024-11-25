7cecce494f7a02c25af8909e342aa6ccdd3d57ff
Nikan Radan <personal@nikanradan.com>
2024-11-25 15:54:07 -0800

    New Config

---
+++ Expanded Mace Enchanting/src/main/kotlin/com/smushytaco/expanded_mace_enchanting/ModConfiguration.kt
@@ -0,0 +1,21 @@
+package com.smushytaco.expanded_mace_enchanting
+import io.wispforest.owo.config.annotation.*
+@Modmenu(modId = ExpandedMaceEnchanting.MOD_ID)
+@Config(name = ExpandedMaceEnchanting.MOD_ID, wrapperName = "ModConfig")
+@Suppress("UNUSED")
+class ModConfiguration {
+    @JvmField
+    var canUseKnockbackOnMace = true
+    @JvmField
+    var canUseLootingOnMace = true
+    @JvmField
+    var canUseSharpnessOnMace = true
+    @JvmField
+    var canUseImpalingOnMace = true
+    @JvmField
+    var densityAndBreachCanBeMixed = true
+    @JvmField
+    var densityAndOtherWeaponEnchantmentTypesCanBeMixed = true
+    @JvmField
+    var breachAndOtherWeaponEnchantmentTypesCanBeMixed = true
+}
