67e95a3934f03c2c967bba9317e9429f8cbad5ff
Nikan Radan <github.com@alias.nikanradan.com>
2025-11-28 22:49:41 -0800

    Official Mappings

--- Expanded Mace Enchanting/src/main/java/com/smushytaco/expanded_mace_enchanting/mixins/MaceModification.java
+++ Expanded Mace Enchanting/src/main/java/com/smushytaco/expanded_mace_enchanting/mixins/MaceModification.java
@@ -1,16 +1,17 @@
 package com.smushytaco.expanded_mace_enchanting.mixins;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.expanded_mace_enchanting.ExpandedMaceEnchanting;
-import net.minecraft.enchantment.*;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.MaceItem;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.core.Holder;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.MaceItem;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import java.util.Set;
 @Mixin(Enchantment.class)
 public abstract class MaceModification {
-    @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
+    @ModifyReturnValue(method = "canEnchant", at = @At("RETURN"))
     public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
         if (ExpandedMaceEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.KNOCKBACK)) {
@@ -28,6 +29,6 @@ public abstract class MaceModification {
         }
         return original;
     }
-    @ModifyReturnValue(method = "canBeCombined", at = @At("RETURN"))
-    private static boolean hookCanBeCombined(boolean original, RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) { return original || ExpandedMaceEnchanting.INSTANCE.getConfig().getDensityAndBreachCanBeMixed() && ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.BREACH)) || ExpandedMaceEnchanting.INSTANCE.getConfig().getDensityAndOtherWeaponEnchantmentTypesCanBeMixed() && (ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.SMITE)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.BANE_OF_ARTHROPODS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.SHARPNESS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.IMPALING))) || ExpandedMaceEnchanting.INSTANCE.getConfig().getBreachAndOtherWeaponEnchantmentTypesCanBeMixed() && (ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.SMITE)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.BANE_OF_ARTHROPODS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.SHARPNESS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.IMPALING))); }
+    @ModifyReturnValue(method = "areCompatible", at = @At("RETURN"))
+    private static boolean hookCanBeCombined(boolean original, Holder<Enchantment> first, Holder<Enchantment> second) { return original || ExpandedMaceEnchanting.INSTANCE.getConfig().getDensityAndBreachCanBeMixed() && ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.BREACH)) || ExpandedMaceEnchanting.INSTANCE.getConfig().getDensityAndOtherWeaponEnchantmentTypesCanBeMixed() && (ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.SMITE)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.BANE_OF_ARTHROPODS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.SHARPNESS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.DENSITY, Enchantments.IMPALING))) || ExpandedMaceEnchanting.INSTANCE.getConfig().getBreachAndOtherWeaponEnchantmentTypesCanBeMixed() && (ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.SMITE)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.BANE_OF_ARTHROPODS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.SHARPNESS)) || ExpandedMaceEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.BREACH, Enchantments.IMPALING))); }
 }
--- Expanded Mace Enchanting/src/main/kotlin/com/smushytaco/expanded_mace_enchanting/ModConfiguration.kt
+++ Expanded Mace Enchanting/src/main/kotlin/com/smushytaco/expanded_mace_enchanting/ModConfiguration.kt
@@ -1,5 +1,6 @@
 package com.smushytaco.expanded_mace_enchanting
-import io.wispforest.owo.config.annotation.*
+import io.wispforest.owo.config.annotation.Config
+import io.wispforest.owo.config.annotation.Modmenu
 @Modmenu(modId = ExpandedMaceEnchanting.MOD_ID)
 @Config(name = ExpandedMaceEnchanting.MOD_ID, wrapperName = "ModConfig")
 @Suppress("UNUSED")
